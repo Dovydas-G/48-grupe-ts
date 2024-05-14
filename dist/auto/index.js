a89d4ca2aa1a87ddde3328492218fbb28af8b1e4
Dovydas-G <146329425+Dovydas-G@users.noreply.github.com>
2024-05-14 15:09:55 +0300

    OOP cars

---
+++ dist/auto/index.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Dealer_js_1 = require("./Dealer.js");
+const bandis = new Dealer_js_1.Dealer('Al Bundy', 'EUR');
+console.log(bandis.getCar('Volvo', 15_000));
+console.log(bandis.getCar('Vokswagen Passat', 20_000));
+console.log(bandis.getCar('Toyota', 17_500));
+console.log(bandis.getCar('Mazda', 10_500));
+console.log(bandis.carList());
+console.log(bandis.changeCarPrice(1, 17_300));
+// New Volvo price is 17.300 EUR.
+console.log(bandis.changeCarPrice(2, 19_500));
+// New Vokswagen Passat price is 19.500 EUR.
+console.log(bandis.carList());
+console.log(bandis.sellCar(1));
+// // Wow! Volvo sold for 17.300 EUR!
+console.log(bandis.fortune());
+console.log(bandis.sellCar(1));
+// console.log(bandis.sellCar(5));
+// // SORRY! There is no such car for sale :(
+console.log(bandis.sellCar(3));
+console.log(bandis.fortune());
+console.log(bandis.sellCar(1));
+console.log(bandis.fortune());
+console.log(bandis.sellCar(1));
+console.log(bandis.fortune());
+console.log(bandis.sellCar(1));
+// console.log(bandis.sellCar(2));
+// // Wow! Toyota sold for 17.500 EUR!
+console.log(bandis.carList());
+console.log('labas');
---
+++ src/auto/Dealer.ts
@@ -0,0 +1,88 @@
+type Car = {
+    model: string,
+    price: number,
+};
+
+export class Dealer {
+    public name: string;
+    public currency: string;
+    public allCars: Car[];
+    public soldCars: Car[];
+
+
+    constructor(name: string, currency: string) {
+        this.currency = currency;
+        this.name = name;
+        this.allCars = [];
+        this.soldCars = [];
+        console.log(`Hi, my name is ${name}!`);
+    }
+
+    formatedPrice(price: number) {
+        return price.toLocaleString('de-DE', {
+            style: 'currency',
+            currency: this.currency
+        });
+    }
+
+    getCar(model: string, price: number) {
+        this.allCars.push({ model, price });
+        return `New car everyone! ${model} for only ${this.formatedPrice(price)}!`;
+    }
+
+    carList() {
+        let result = [`${this.name}\'s car dealership:`];
+
+        for (let i = 0; i < this.allCars.length; i++) {
+            const car = this.allCars[i];
+            const price = this.formatedPrice(car.price);
+            const ending = i < this.allCars.length - 1 ? ';' : '.';
+            result.push(`${i + 1}) ${car.model}: ${price}${ending}`);
+        }
+
+        const count = Math.max(...result.map(row => row.length));
+        result.splice(1, 0, '='.repeat(count));
+
+        return result.join('\r\n');
+    }
+
+    changeCarPrice(id: number, newPrice: number) {
+        if (id < 1) {
+            return 'ERROR: id is too low.';
+        }
+        if (id > this.allCars.length) {
+            return 'ERROR: id is too big.';
+        }
+
+        const car = this.allCars[id - 1];
+        car.price = newPrice;
+        return `New ${car.model} price is ${this.formatedPrice(newPrice)}.`;
+    }
+
+    sellCar(id: number) {
+        const carId = id - 1;
+        const car = this.allCars[carId];
+
+        if (id < 1 || id > this.allCars.length) {
+            return 'SORRY! There is no such car for sale :(';
+        }
+        
+        this.soldCars.push(car);
+        this.allCars.splice(carId, 1);
+        
+        return `Wow! ${car.model} sold for ${this.formatedPrice(car.price)}`;
+    }
+
+    fortune() {
+        let countSoldCars = 0;
+        let total = 0;
+
+        for (let i = 0; i < this.soldCars.length; i++) {
+            const sold = this.soldCars[i];
+            total += sold.price;
+            countSoldCars++;
+        }
+    
+        return `${this.name} has sold ${countSoldCars} cars for total of ${this.formatedPrice(total)}`;
+    }
+}
